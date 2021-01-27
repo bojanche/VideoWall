f148497df3d425921da38c99ad4eb27ecd3cbf18
bojanche <Boj@n@25122015>
2021-01-27 07:29:24 +0100

    1.0.18

--- VideoWall/playgorund.py
+++ VideoWall/playgorund.py
@@ -5,7 +5,7 @@ import sys
 
 #
 try:
-    in_stream = ffmpeg.probe('./tmp_storage/pitch.mp4')
+    in_stream = ffmpeg.probe('./tmp_storage/1080p.mp4')
 except ffmpeg.Error as e:
     print(e.stderr, file=sys.stderr)
     sys.exit(1)
@@ -16,11 +16,11 @@ if video_stream is None:
     print('No video stream found', file=sys.stderr)
     sys.exit(1)
 
-in_stream = ffmpeg.input('./tmp_storage/pitch.mp4')
+in_stream = ffmpeg.input('./tmp_storage/1080p.mp4')
 
 ### High stream
 out_stream_high = ffmpeg.output(in_stream, './tmp_storage/high/pitch.m3u8', format='hls', start_number=0,
-                                hls_time=6, hls_list_size=0, vcodec='libx265', crf=26, **{'qscale:v': 3})
+                                hls_time=6, hls_list_size=0, vcodec='libx265', crf=25, **{'qscale:v': 3})
 high = threading.Thread(target=ffmpeg.run, args=(out_stream_high,))
 high.start()
 ### End High Stream
@@ -28,8 +28,8 @@ high.start()
 # Testing for file reduction size
 
 ### Low Stream
-out_stream_low = ffmpeg.filter(in_stream, filter_name='scale', size='640x320').output(in_stream, './tmp_storage/low/pitch.m3u8', format='hls', start_number=0, hls_time=10,
-                           hls_list_size=0, vcodec='libx265', crf=27, **{'qscale:v': 3})
+out_stream_low = ffmpeg.filter(in_stream, filter_name='scale', size='640x320').output(in_stream, './tmp_storage/low/pitch.m3u8', format='hls', start_number=0, hls_time=6,
+                           hls_list_size=0, vcodec='libx265', crf=28, **{'qscale:v': 3})
 low = threading.Thread(target=ffmpeg.run, args=(out_stream_low, ))
 low.start()
 ### End Low Stream
